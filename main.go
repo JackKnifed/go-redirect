df57700548f461637bdaaa173c028813417a746d
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2015-08-25 20:52:18 -0400

    added the final binary to the gitignore and fixed all errors in main.go

--- main.go
+++ main.go
@@ -3,30 +3,31 @@ package main
 import (
 	"net/http"
 	"flag"
+	"log"
 )
 
-listenPort := flag.Int("port", 80, "port to listen on")
-tlsCert := flag.String("tlsCert", "", "If using TLS, cert to use")
-tlsKey := flag.String("tlsKey", "", "If using TLS, key to use")
-bindAddress := flag.String("bind", "0.0.0.0", "address to listen to")
+var listenPort = flag.Int("port", 80, "port to listen on")
+var tlsCert = flag.String("tlsCert", "", "If using TLS, cert to use")
+var tlsKey = flag.String("tlsKey", "", "If using TLS, key to use")
+var bindAddress = flag.String("bind", "0.0.0.0", "address to listen to")
 
-resEncrypt := flag.Bool("https", true, "determine whether or not to include encryption in the redirect target")
-resPort := flag.Int("targetPort", 443, "port to direct the request towards")
-resCode := flag.Int("status", 301, "status code to return to the client")
+var resEncrypt = flag.Bool("https", true, "determine whether or not to include encryption in the redirect target")
+var resPort = flag.Int("targetPort", 443, "port to direct the request towards")
+var resCode = flag.Int("status", 301, "status code to return to the client")
 
 func redir(w http.ResponseWriter, req *http.Request) {
 
-	if resEncrypt {
-		if resPort == 443 {
-			http.Redirect(w, req, "https://"+req.Host+req.RequestURI, resCode)
+	if *resEncrypt {
+		if *resPort == 443 {
+			http.Redirect(w, req, "https://"+req.Host+req.RequestURI, *resCode)
 		} else {
-			http.Redirect(w, req, "https://"+req.Host:":"resPort+req.RequestURI, resCode)
+			http.Redirect(w, req, "https://"+req.Host+":" + string(*resPort)+req.RequestURI, *resCode)
 		}
 	} else {
-		if resPort == 80 {
-			http.Redirect(w, req, "http://"+req.Host+req.RequestURI, resCode)
+		if *resPort == 80 {
+			http.Redirect(w, req, "http://" + req.Host + req.RequestURI, *resCode)
 		} else {
-			http.Redirect(w, req, "http://"+req.Host:":"resPort+req.RequestURI, resCode)
+			http.Redirect(w, req, "http://" + req.Host + ":" + string(*resPort) + req.RequestURI, *resCode)
 		}
 	}
 }
@@ -34,12 +35,12 @@ func redir(w http.ResponseWriter, req *http.Request) {
 func main() {
 	flag.Parse()
 
-	if tlsCert != "" && tlsKey != "" {
-		if err := http.ListenAndServeTLS(string(bindAddress)+":"+listenPort, certFile, keyFile, handler); err != nil {
+	if *tlsCert != "" && *tlsKey != "" {
+		if err := http.ListenAndServeTLS(string(*bindAddress)+":"+string(*listenPort), *tlsCert, *tlsKey, http.HandlerFunc(redir)); err != nil {
 			log.Println(err)
 		}
 	} else {
-		if err := http.ListenAndServe(string(bindAddress)+":"+listenPort, handler); err != nil {
+		if err := http.ListenAndServe(string(*bindAddress)+":"+string(*listenPort), http.HandlerFunc(redir)); err != nil {
 			log.Println(err)
 		}
 	}
