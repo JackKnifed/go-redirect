9468b847059fffb2d2bea4538e22cbefaefb3f49
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2015-08-25 20:43:44 -0400

    finished writing the initial stuff, no checking for the flags yet though

--- main.go
+++ main.go
@@ -1,13 +1,46 @@
 package main
 
 import (
-			"net/http"
-		)
+	"net/http"
+	"flag"
+)
+
+listenPort := flag.Int("port", 80, "port to listen on")
+tlsCert := flag.String("tlsCert", "", "If using TLS, cert to use")
+tlsKey := flag.String("tlsKey", "", "If using TLS, key to use")
+bindAddress := flag.String("bind", "0.0.0.0", "address to listen to")
+
+resEncrypt := flag.Bool("https", true, "determine whether or not to include encryption in the redirect target")
+resPort := flag.Int("targetPort", 443, "port to direct the request towards")
+resCode := flag.Int("status", 301, "status code to return to the client")
 
 func redir(w http.ResponseWriter, req *http.Request) {
-		http.Redirect(w, req, "https://"+req.Host+req.RequestURI, http.StatusMovedPermanently)
+
+	if resEncrypt {
+		if resPort == 443 {
+			http.Redirect(w, req, "https://"+req.Host+req.RequestURI, resCode)
+		} else {
+			http.Redirect(w, req, "https://"+req.Host:":"resPort+req.RequestURI, resCode)
+		}
+	} else {
+		if resPort == 80 {
+			http.Redirect(w, req, "http://"+req.Host+req.RequestURI, resCode)
+		} else {
+			http.Redirect(w, req, "http://"+req.Host:":"resPort+req.RequestURI, resCode)
+		}
+	}
 }
 
 func main() {
-		http.ListenAndServe(":8080", http.HandlerFunc(redir))
+	flag.Parse()
+
+	if tlsCert != "" && tlsKey != "" {
+		if err := http.ListenAndServeTLS(string(bindAddress)+":"+listenPort, certFile, keyFile, handler); err != nil {
+			log.Println(err)
+		}
+	} else {
+		if err := http.ListenAndServe(string(bindAddress)+":"+listenPort, handler); err != nil {
+			log.Println(err)
+		}
+	}
 }
